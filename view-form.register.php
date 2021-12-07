8a4c12a5c87596ea185a69c413e43b46edc5596e
Felipe <felipesms@live.com>
2021-12-07 02:54:29 -0300

    Testing deploy automation

--- view-form.register.php
+++ view-form.register.php
@@ -7,8 +7,14 @@
             <input type="text" name="" class="form-control">
             <label for="">Usuário</label>
             <input type="text" name="" class="form-control">
+            <label for="">Senha</label>
+            <input type="password" name="" class="form-control">
+            <label for="">Confirmar Senha</label>
+            <input type="password" name="" class="form-control">
+
             <div class="d-grid gap-2 mt-2">
                 <button class="btn btn-primary btn-block">Registrar</button>
             </div>
             
         </form>
+        
