b0c2278ebba092c7e811e29a3398584619222d49
Felipe <felipesms@live.com>
2021-12-07 03:08:10 -0300

    Adding TOS and forgot password button

--- view-form.register.php
+++ view-form.register.php
@@ -12,9 +12,16 @@
             <label for="">Confirmar Senha</label>
             <input type="password" name="" class="form-control">
 
-            <div class="d-grid gap-2 mt-2">
+            <div class="d-grid gap-2 mt-2 mb-2">
                 <button class="btn btn-primary btn-block">Registrar</button>
             </div>
-            
+            <input type="checkbox" name="" id=""> 
+                <small>Aceito os termos de uso da plataforma</small> 
+            <div>
+                <button class="btn-primary btn">
+                    <small>
+                        Esqueci minha senha
+                    </small>
+                </button>
+            </div>
         </form>
-        
