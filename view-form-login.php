f1d87b3c6efa09007a88ea74d06b27e0ca9c82cc
Felipe <felipesms@live.com>
2021-12-07 08:11:42 -0300

    Form login development

--- view-form-login.php
+++ view-form-login.php
@@ -1,5 +1,8 @@
-<button onclick="forgotPassView()" class="btn-primary btn" data-bs-toggle="modal" data-bs-target="#genericModal">
-                    <small>
-                        Esqueci minha senha
-                    </small>
-                </button>
+
+
+    <input type="text" name="username" class="form-control" id="">
+    <button onclick="forgotPassView()" class="btn-primary btn" data-bs-toggle="modal" data-bs-target="#genericModal">
+        <small>
+            Esqueci minha senha
+        </small>
+    </button>
