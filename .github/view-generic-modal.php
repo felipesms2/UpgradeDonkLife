13a5aace5d12a9eb66a9c7a1f926c98595e5c2e9
Felipe <felipesms@live.com>
2021-12-07 03:13:19 -0300

    Including id properties on generic modal

--- .github/view-generic-modal.php
+++ .github/view-generic-modal.php
@@ -6,7 +6,7 @@
         <h5 class="modal-title" id="modalTitle">Modal title</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-      <div class="modal-body">
+      <div class="modal-body" id="modal-body">
     
       </div>
       <div class="modal-footer">
