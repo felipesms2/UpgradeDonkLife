bd47a8fc285b5c6b71a5c74ee211d8f036b7c36c
Felipe <felipesms@live.com>
2021-12-07 08:08:18 -0300

    Form login view created

---
+++ view-form-login.php
@@ -0,0 +1,5 @@
+<button onclick="forgotPassView()" class="btn-primary btn" data-bs-toggle="modal" data-bs-target="#genericModal">
+                    <small>
+                        Esqueci minha senha
+                    </small>
+                </button>
