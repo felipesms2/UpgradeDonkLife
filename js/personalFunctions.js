e76cd347f3abb91d2a33245c6bee1a2f8a19becb
Felipe <felipesms@live.com>
2021-12-07 04:02:31 -0300

    Deveolping function forgot password

--- js/personalFunctions.js
+++ js/personalFunctions.js
@@ -6,5 +6,7 @@ function modifyModalData(metaData)
 
 function forgotPassView() 
     {
+        let metadata =[];
+        metadata["title"] = "Resetar Senha";
         alert("modificar modal");
     }
