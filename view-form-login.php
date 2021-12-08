3c84bb33ff6f69767cd8db7980b4813193a930fe
Felipe <felipesms@live.com>
2021-12-08 05:34:32 -0300

    Fixing button positions

--- view-form-login.php
+++ view-form-login.php
@@ -1,6 +1,8 @@
 
-
+    <label for="">Usuário</label>
     <input type="text" name="username" class="form-control" id="">
+    <label for="">Senha</label>
+    <input type="password" name="passwd" class="form-control mb-2" id="">
     <button onclick="forgotPassView()" class="btn-primary btn" data-bs-toggle="modal" data-bs-target="#genericModal">
         <small>
             Esqueci minha senha
