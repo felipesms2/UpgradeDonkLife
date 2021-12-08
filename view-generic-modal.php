0e2d1879980c9fb9bc7dd080fff27dd63253f495
Felipe <felipesms@live.com>
2021-12-08 06:33:26 -0300

    Adde modal div for fill

--- view-generic-modal.php
+++ view-generic-modal.php
@@ -7,7 +7,9 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body" id="modal-body">
-    
+        <div id="form-modal-body">
+          
+        </div>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
