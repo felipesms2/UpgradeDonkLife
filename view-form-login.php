eeb784656498ec342da85560c98173e33eb48b67
Felipe <felipesms@live.com>
2021-12-11 10:57:16 -0300

    Fixing bug by lack of id attribute username

--- view-form-login.php
+++ view-form-login.php
@@ -1,6 +1,6 @@
 
     <label for="">Usuário</label>
-    <input type="text" name="username" class="form-control" id="">
+    <input type="text" name="username" class="form-control" id="username">
     <label for="">Senha</label>
     <input type="password" name="passwd" class="form-control mb-2" id="">
     <button onclick="forgotPassView()" class="btn-primary btn" data-bs-toggle="modal" data-bs-target="#genericModal">
