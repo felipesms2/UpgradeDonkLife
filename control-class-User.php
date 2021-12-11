5b66646072c9e62a77ebd344460ac5e321bee4df
Felipe <felipesms@live.com>
2021-12-11 16:31:07 -0300

    Generating geral variables

---
+++ control-class-User.php
@@ -0,0 +1,17 @@
+
+<?php
+
+class User
+    
+    {
+        public $name;
+        public $email;
+        public $userName;
+        public $password;
+        public $passwordConfirm;
+
+        public function Register()
+        {
+            
+        }
+    }
