8b4277936140a54651c2c4706673ab5d8e461162
Felipe <felipesms@live.com>
2021-12-11 16:48:40 -0300

    Setting db

--- control-class-User.php
+++ control-class-User.php
@@ -4,13 +4,20 @@
 class User
     
     {
+        public $pdoConn;
         public $name;
         public $email;
         public $userName;
         public $password;
         public $passwordConfirm;
+        public $checkRegister;
 
-        public function Register()
+        private function __construct($dbObj)
+            {
+                $this->pdoConn = $dbObj;
+            }
+        
+        public function userAvailability()
         {
             
         }
