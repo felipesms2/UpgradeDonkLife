1b37b71544c3adba5e9f32881976da862be56ec8
felipesms2 <felipesms@live.com>
2021-12-14 12:07:11 -0300

    Constructing form validations

--- control-form-actions.php
+++ control-form-actions.php
@@ -4,9 +4,56 @@
     $postString = implode(",", $_POST);
     $action = $_POST['action'];    
 
-    switch ($action) {
+    switch ($action) 
+    {
         case 'registration':
-            $postString .= $action;    
+            
+            $error ="";
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $userName = $_POST['userName'];
+            $password = $_POST['password'];
+            $passwordConfirm = $_POST['passwordConfirm'];
+            if (empty($name)) 
+                {
+                    $error .= "<li>Nome</li>";
+                }
+
+            if (empty($email)) 
+                {
+                    $error .= "<li>Email</li>";
+                }
+
+            if (empty($userName)) 
+                {
+                    $error .= "<li>Email</li>";
+                }
+
+            if (empty($password)) 
+                {
+                    $error .= "<li>Senha não pode ficar vazia</li>";
+                }
+
+            if (empty($passwordConfirm)) 
+                {
+                    $error .= "<li>Confirmação de senha ausente</li>";
+                }
+            if ($password !== $passwordConfirm) 
+                {
+                    $error .= "<li>Senha e confirmação de senha devem ser iguais</li>";
+                }
+            if (empty($error)) 
+            {
+                http_response_code( 200 );
+                echo json_encode( [ 'msg' => "ok" ] );
+            }
+                
+            else
+            {
+                http_response_code( 406 );
+                echo json_encode( [ 'msg' => $error ] );
+            }
+            
         break;
         
         default:
@@ -16,5 +63,5 @@
 
 
 
-    echo json_encode( [ 'msg' => $postString ] );
+    
     
