3e8490e38089b9f0f5cf59ebb000a2bfd065a33c
felipesms2 <felipesms@live.com>
2021-12-14 12:06:09 -0300

    Changing typeof email input

--- view-form-register.php
+++ view-form-register.php
@@ -1,22 +1,26 @@
 
-            <form name="registrationForm" id="registrationForm" method="POST">
+            <form 
+                name="registrationForm"
+                id="registrationForm"
+                method="POST"
+                action="./control-form-actions.php";
+                >
             <input type="hidden" name="action" value="registration"/>
         
             <label for="userFriendlyName">Nome</label>
             <input type="text" name="name" class="form-control">
             <label for="">Email</label>
-            <input type="text" name="email" class="form-control">
+            <input type="email" name="email" class="form-control">
             <label for="">Usuário</label>
             <input type="text" name="userName" class="form-control">
             <label for="">Senha</label>
             <input type="password" name="password" class="form-control">
             <label for="">Confirmar Senha</label>
-            <input type="passwordConfirm" name="" class="form-control">
+            <input type="password" name="passwordConfirm" name="" class="form-control">
 
             <div class="d-grid gap-2 mt-2 mb-2">
                 <button 
                     class="btn btn-primary btn-block"
-                    type="submit"
                     id="btnSubmit"
                     >Registrar
                 </button>
