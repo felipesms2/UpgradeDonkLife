384740a8af26998e993aedf8bbf19fc51f83290a
felipesms2 <felipesms@live.com>
2021-12-17 17:15:20 -0300

    Including phpmMailerLibrary

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registrar</title>
     <?php
+        include "./vendor/autoload.php";
         include "./assets-bunch.php";
     ?>
 </head>
