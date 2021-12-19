bd65322596f05a95993af750519b4fb7a0c27c57
felipesms2 <felipesms@live.com>
2021-12-18 23:51:39 -0300

    Solving void var

--- model-config.php
+++ model-config.php
@@ -2,6 +2,11 @@
 
     //$typeRequest = $_SERVER["REQUEST_SCHEME"];
     //$mainSiteUrl = $_SERVER['HTTP_HOST'];
+
+    if (!isset($_SERVER['HTTP_HOST'])) 
+    {
+        $_SERVER['HTTP_HOST'] = "";
+    }
     define("SCRIPT_FOLDER", $_SERVER['HTTP_HOST']);
 
     
