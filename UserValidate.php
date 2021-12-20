042538d774aca40f739518dbdbca07a2f451983d
felipesms2 <felipesms@live.com>
2021-12-20 13:51:42 -0300

    changing return for user validate

--- UserValidate.php
+++ UserValidate.php
@@ -1,1 +1,35 @@
 
+<?php
+
+include "./model-config.php";
+include "./control-class-User.php";
+include "./model-db-control.php";
+
+function redir()
+{
+    header("location: index.php");
+}
+
+
+if (isset ($_GET['action'])) 
+    {
+        $action = $_GET['action'];
+        switch ($action)
+        {
+            case 'checkNewUser':
+                    $token = $_GET['token'];            
+                    $user = new User($pdo);                    
+                    $user->user_actkey = $token;
+                    echo $user->checkActivation();
+            break;
+            
+            default:
+                redir();
+            break;
+        }
+    }
+else
+
+    {
+        redir();
+    }
