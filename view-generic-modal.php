941ff8ec08be6558fd03901ff015d6ed8f251fb8
felipesms2 <felipesms@live.com>
2021-12-21 18:54:39 -0300

    Changing button title

--- view-generic-modal.php
+++ view-generic-modal.php
@@ -12,7 +12,7 @@
         </div>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
         <div id="btnModalAction">
           <button type="button" class="btn btn-primary"  onclick="callReset()" id='modalBtnSave'>Resetar Senha</button>
         </div>
