ac2e91ebb1a803a06df72808aef0f2362e104d62
felipesms2 <felipesms@live.com>
2021-12-20 21:43:01 -0300

    Adding action hidden input

--- view-form-forgot.php
+++ view-form-forgot.php
@@ -1,10 +1,15 @@
 
     Email
     <form id="formForgot">
+    <input type="hidden" name="action" id="action" value="askReset">
     <input 
         type="text" 
         class="form-control"
         placeholder="Digite seu email aqui" 
-        name="email" id="emailForgot"
+        name="emailForgot" id="emailForgot"
     >
     </form>
+
+    <div id="alertReset" class ="text-center mt-2">
+        Teste de texto
+    </div>
