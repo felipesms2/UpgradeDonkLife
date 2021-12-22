b024c81c8fcc8338fd69ab0f74d52e47e63e9126
felipesms2 <felipesms@live.com>
2021-12-22 00:02:14 -0300

    Form reset auth modifications

--- view-form-reset-auth.php
+++ view-form-reset-auth.php
@@ -1,7 +1,9 @@
 
-
+    <form id="resetAuth" method="post" action="./control-form-actions.php">
+    <input type="hidden" name="action" value="resetConfirm">    
     Nova Senha
     <input class="form-control" type="password" name="password" id="password">
     Confirmar Senha
     <input class="form-control" type="passworr" name="passwordConfirm" id="passwordConfirm" >
     <button class="form-control btn-success mt-4">Confirmar nova senha</button>
+    </form>
