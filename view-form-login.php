1a76552b858aed3148750acb6c20495a801a0411
felipesms2 <felipesms@live.com>
2022-01-06 20:57:47 -0300

    testing error validation

--- view-form-login.php
+++ view-form-login.php
@@ -2,7 +2,7 @@
 <form 
     id="loginRules"
     method="POST"
-    action="../forum-homolog/index.php"    
+    action="../../forum-homolog/index.php"    
     
 >
 <!-- action="control-form-actions.php" -->
